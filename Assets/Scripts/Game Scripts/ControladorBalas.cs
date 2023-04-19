3d7154493fe336c947bbfcca96e35b74b5cb5e2b
IgnaZ10 <64873313+IgnaZ10@users.noreply.github.com>
2023-04-18 22:06:08 -0300

    Implementacion parcial de sonidos
    Aun faltan los sliders de volumen, pero ya esta asignada la musica y los SFX a un audio mixer y los sonidos se reproducen como deberian

--- Assets/Scripts/Game Scripts/ControladorBalas.cs
+++ Assets/Scripts/Game Scripts/ControladorBalas.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class ControladorBalas : MonoBehaviour
 {
     [SerializeField] private Transform controladorDisparo;
     [SerializeField] private GameObject bala;
-
+    public AudioSource Disparo;
+    public AudioClip Shoot;
+    
     private void Update()
     {
         if (GameManager.Instance.gameIsPaused) return; // Ignorar Input si el juego está pausado
@@ -19,6 +22,7 @@ public class ControladorBalas : MonoBehaviour
     void Disparar() 
     {
         Instantiate(bala, controladorDisparo.position, controladorDisparo.rotation);
+        Disparo.PlayOneShot(Shoot);
     }
 
 }
---
+++ Assets/Scripts/Game Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    [SerializeField] private AudioSource _musicSource, _SFXSource;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        _SFXSource.PlayOneShot(clip);
+    
+    }
+}
